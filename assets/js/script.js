a95611b4f095d14cc8b4644b245cb8cae85e2388
umanrai <umanrai58@gmail.com>
2024-03-04 17:38:37 +0545

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const locationResult = document.querySelector('#locationResult');
+document.querySelector('#getLocation').addEventListener('click', () => {
+    locationResult.textContent = 'Retrieving User Location...'
+
+    function success(position) {
+        let { coords } = position;
+        locationResult.textContent = 'See my location on a map';
+        locationResult.href = `https://www.openstreetmap.org?mlat=${coords.latitude}&mlon=${coords.longitude}`;
+    }
+
+    navigator.geolocation.getCurrentPosition(success);
+});
